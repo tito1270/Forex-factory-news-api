41d6ff8c8d71ac44da5a6ea9697004ce3d3fbd08
tito1270 <156559344+tito1270@users.noreply.github.com>
2025-06-06 17:07:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+@app.route('/')
+def home():
+    return "Forex Factory News API is running!"
